cfb8dbe1748d5b4f16f01dfddddb601b0bbb41d3
Phucluu2809 <thanhtam.141019830@gmail.com>
2026-05-02 17:38:42 +0700

    feat: build spotify mini mobile player ui

--- spotifyMini/app/(tabs)/_layout.tsx
+++ spotifyMini/app/(tabs)/_layout.tsx
@@ -1,35 +1,83 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Ionicons } from "@expo/vector-icons";
+
+import MiniPlayer from "../../components/MiniPlayer";
+
+import { View } from "react-native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+    <View style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "#121212",
+            borderTopColor: "#222"
+          },
+          tabBarActiveTintColor: "#1DB954",
+          tabBarInactiveTintColor: "gray"
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="home"
+                color={color}
+                size={size}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: "Search",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="search"
+                color={color}
+                size={size}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="library"
+          options={{
+            title: "Library",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="library"
+                color={color}
+                size={size}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="settings"
+                color={color}
+                size={size}
+              />
+            )
+          }}
+        />
+      </Tabs>
+
+      <MiniPlayer />
+    </View>
   );
 }
